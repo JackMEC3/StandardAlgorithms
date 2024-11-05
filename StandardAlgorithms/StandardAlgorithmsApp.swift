4b44c8069a251d1e8f90ab730885ef3c80f4f65b
Jack Ma <ma.j@etoncollege.org.uk>
2024-11-05 10:05:54 +0000

    Initial Commit

---
+++ StandardAlgorithms/StandardAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithmsApp.swift
+//  StandardAlgorithms
+//
+//  Created by Ma, Jack (IRG) on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StandardAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
