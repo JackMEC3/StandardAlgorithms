990049021d293028056ad4b35b9b0dbadd2dc820
Jack Ma <ma.j@etoncollege.org.uk>
2024-11-14 19:01:18 +0000

    Quick sort completed, others half done.

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -7,61 +7,70 @@
 
 import Foundation
 class Sorting{
-    func bubbleSort(_ numbers: [Int]) -> [Int] {
-        for i in 1...numbers.count{
-            var number = sorted[i]
-            for j in i...sorted.count{
-                if number < sorted[j]{
-                    sorted[j] = number
-                }
-            }
-            
-        }
-        for i in 1..<numbers.count{
-            var number = numbers[i]
-            group.append(number)
-            sorted.append(group)
-            for i in 1...sorted.count{
-                if sorted[i][0]
-                    
-                    
-            }
-            
-        }
-    }
-    func mergeSort(_ numbers: [Int]) -> [Int] {
-        var sorted: [[Int]] = []
-        var group: [Int] = []
-        for i in 1..<numbers.count{
-            var number = numbers[i]
-            group.append(number)
-            sorted.append(group)
-            for i in 1...sorted.count{
-                if sorted[i][0] {
-                    
-                }
-            }
-        }
-        
-    }
+    //    func bubbleSort(_ numbers: [Int]) -> [Int] {
+    //        for i in 1...numbers.count{
+    //            var number = sorted[i]
+    //            for j in i...sorted.count{
+    //                if number < sorted[j]{
+    //                    sorted[j] = number
+    //                }
+    //            }
+    //
+    //        }
+    //        for i in 1..<numbers.count{
+    //            var number = numbers[i]
+    //            group.append(number)
+    //            sorted.append(group)
+    //            for i in 1...sorted.count{
+    //                if sorted[i][0]
+    //
+    //
+    //            }
+    //
+    //        }
+    //    }
+    //    func mergeSort(_ numbers: [Int]) -> [Int] {
+    //        var sorted: [[Int]] = []
+    //        var group: [Int] = []
+    //        for i in 1..<numbers.count{
+    //            var number = numbers[i]
+    //            group.append(number)
+    //            sorted.append(group)
+    //            for i in 1...sorted.count{
+    //                if sorted[i][0] {
+    //
+    //                }
+    //            }
+    //        }
+    //
+    //    }
+    
+    
+    
+    
+    
     func quickSort(_ numbers: [Int]) -> [Int] {
-        var pivot = numbers[0]
-        var lower: [Int] = []
-        var higher: [Int] = []
-        for i in 0...numbers.count {
-            var number = numbers[i]
-            if number < pivot {
-                lower.append(numbers[i])
-            }
-            else if numbers[i] > pivot{
-                higher.append(numbers[i])
+        switch numbers.count {
+        case 0:
+            return []
+        case 1:
+            return numbers
+        default:
+            let pivot = numbers[0]
+            var lower: [Int] = []
+            var higher: [Int] = []
+            
+            for i in 1..<numbers.count {
+                let number = numbers[i]
+                if number < pivot {
+                    lower.append(number)
+                } else {
+                    higher.append(number)
                 }
             }
+            
+            return quickSort(lower) + [pivot] + quickSort(higher)
         }
-        
-        let numbers = []
-        return numbers
-        
-        
     }
+    
 }
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -9,17 +9,17 @@ import XCTest
 @testable import StandardAlgorithms
 
 final class SortingTest: XCTestCase {
-    func testBubleSortWithariousIntegerArraysReturnsArraysInAscendingOrder() {
-        let sorting = Sorting()
-        let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
-                         (numbers: [1,2,3], expected: [1,2,3])]
-        for testCase in testCases {
-            let result = sorting.bubbleSort(testCase.numbers)
-            XCTAssertEqual(result, testCase.expected)
-        }
+//    func testBubleSortWithariousIntegerArraysReturnsArraysInAscendingOrder() {
+//        let sorting = Sorting()
+//        let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
+//                         (numbers: [1,2,3], expected: [1,2,3])]
+//        for testCase in testCases {
+//            let result = sorting.bubbleSort(testCase.numbers)
+//            XCTAssertEqual(result, testCase.expected)
+//        }
         
     }
-    func testQuickSortWithariousIntegerArraysReturnsArraysInAscendingOrder() {
+    func testQuickSortWithIntegerArraysReturnsArraysInAscendingOrder() {
         let sorting = Sorting()
         let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
                          (numbers: [1,2,3], expected: [1,2,3])]
@@ -28,6 +28,5 @@ final class SortingTest: XCTestCase {
             XCTAssertEqual(result, testCase.expected)
         }
     }
-    testQuickSortWithariousIntegerArraysReturnsArraysInAscendingOrder()
-    
-}
+ 
+
