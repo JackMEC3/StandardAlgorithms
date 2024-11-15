a3e53ba8d113b498e7e6595cfb79958074b89e65
Jack Ma <ma.j@etoncollege.org.uk>
2024-11-15 17:09:50 +0000

    Finished bubble and quick sort, working on merge sort.

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -7,43 +7,42 @@
 
 import Foundation
 class Sorting{
-    //    func bubbleSort(_ numbers: [Int]) -> [Int] {
-    //        for i in 1...numbers.count{
-    //            var number = sorted[i]
-    //            for j in i...sorted.count{
-    //                if number < sorted[j]{
-    //                    sorted[j] = number
-    //                }
-    //            }
-    //
-    //        }
-    //        for i in 1..<numbers.count{
-    //            var number = numbers[i]
-    //            group.append(number)
-    //            sorted.append(group)
-    //            for i in 1...sorted.count{
-    //                if sorted[i][0]
-    //
-    //
-    //            }
-    //
-    //        }
-    //    }
-    //    func mergeSort(_ numbers: [Int]) -> [Int] {
-    //        var sorted: [[Int]] = []
-    //        var group: [Int] = []
-    //        for i in 1..<numbers.count{
-    //            var number = numbers[i]
-    //            group.append(number)
-    //            sorted.append(group)
-    //            for i in 1...sorted.count{
-    //                if sorted[i][0] {
-    //
-    //                }
-    //            }
-    //        }
-    //
-    //    }
+        func bubbleSort(_ numbers: [Int]) -> [Int] {
+            var sorted = numbers
+            for i in 0..<sorted.count{
+                var number1 = sorted[i]
+                var number2 = sorted[i+1]
+                if number2 < number1{
+                    sorted[i+1] = number1
+                    sorted[i+1] = number2
+                }
+            }
+            return sorted
+    
+            }
+
+        func mergeSort(_ numbers: [Int]) -> [Int] {
+            func divide(_ array: [Int]) -> [Int] {
+                var arr = array
+                var arr1: [Int] = []
+                var arr2: [Int] = []
+            }
+            var sorted = numbers
+            for i in
+
+            var group: [Int] = []
+            for i in 1..<numbers.count{
+                var number = numbers[i]
+                group.append(number)
+                sorted.append(group)
+                for i in 1...sorted.count{
+                    if sorted[i][0] {
+    
+                    }
+                }
+            }
+    
+        }
     
     
     
--- StandardAlgorithmsTests/SortingTest.swift
+++ StandardAlgorithmsTests/SortingTest.swift
@@ -9,24 +9,35 @@ import XCTest
 @testable import StandardAlgorithms
 
 final class SortingTest: XCTestCase {
-//    func testBubleSortWithariousIntegerArraysReturnsArraysInAscendingOrder() {
-//        let sorting = Sorting()
-//        let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
-//                         (numbers: [1,2,3], expected: [1,2,3])]
-//        for testCase in testCases {
-//            let result = sorting.bubbleSort(testCase.numbers)
-//            XCTAssertEqual(result, testCase.expected)
-//        }
-        
-    }
-    func testQuickSortWithIntegerArraysReturnsArraysInAscendingOrder() {
+    func testBubbleSortWithariousIntegerArraysReturnsArraysInAscendingOrder() {
         let sorting = Sorting()
         let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
                          (numbers: [1,2,3], expected: [1,2,3])]
         for testCase in testCases {
-            let result = sorting.quickSort(testCase.numbers)
+            let result = sorting.bubbleSort(testCase.numbers)
             XCTAssertEqual(result, testCase.expected)
         }
+        
+        func testMergeSortWithIntegerArraysReturnsArraysInAscendingOrder() {
+            let sorting = Sorting()
+            let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
+                             (numbers: [1,2,3], expected: [1,2,3])]
+            for testCase in testCases {
+                let result = sorting.mergeSort(testCase.numbers)
+                XCTAssertEqual(result, testCase.expected)
+            }
+            
+        }
+        func testQuickSortWithIntegerArraysReturnsArraysInAscendingOrder() {
+            let sorting = Sorting()
+            let testCases = [(numbers: [4, 1, 7, 9, 3], expected: [1, 3, 4, 7, 9]),                        (numbers: [4, 14, 9, 0 ,1], expected: [0, 1, 4, 9, 14]),
+                             (numbers: [1,2,3], expected: [1,2,3])]
+            for testCase in testCases {
+                let result = sorting.quickSort(testCase.numbers)
+                XCTAssertEqual(result, testCase.expected)
+            }
+        }
+        
+        
     }
- 
-
+}
